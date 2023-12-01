337119569c1db44c5060cc1f1a77de5e61d209dd
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-12-01 21:10:46 +0100

    Added Statistics class

---
+++ FilmChallengeApp/FilmChallengeApp/Statistics.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmChallengeApp;
+
+public class Statistics
+{
+    public float Min { get; set; }
+    public float Max { get; set; }
+    public float Average { get; set; }
+    public float AverageLetter { get; set; }
+    public float Sum { get; set; }
+    public float Count { get; set; }
+}
