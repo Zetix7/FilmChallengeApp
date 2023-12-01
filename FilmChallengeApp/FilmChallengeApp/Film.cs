b2e5e79ecc5cde94a7b49b47f3ecad497081c7ce
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-12-01 21:08:58 +0100

    Added Film class, 3 AddGrade methods

---
+++ FilmChallengeApp/FilmChallengeApp/Film.cs
@@ -0,0 +1,49 @@
+﻿namespace FilmChallengeApp;
+
+public class Film
+{
+    private List<int> _grades = new();
+
+    public Film(string title, int year)
+    {
+        Title = title;
+        Year = year;
+    }
+
+    public string Title { get; set; }
+    public int Year { get; set; }
+
+    public void AddGrade(int grade)
+    {
+        if (grade >=1 && grade <= 100)
+        {
+            _grades.Add(grade);
+        }
+        else
+        {
+            throw new ArgumentException($"Invalid value: '{grade}'! Correct value range is from 1 to 100.");
+        }
+    }
+
+    public void AddGrade(string grade)
+    {
+        if (int.TryParse(grade, out int intResult))
+        {
+            AddGrade(intResult);
+        }
+        else if (float.TryParse(grade, out float floatResult))
+        {
+            AddGrade(floatResult);
+        }
+        else
+        {
+            throw new FormatException($"Invalid value: '{grade}'! Value is not int.");
+        }
+    }
+
+    public void AddGrade(float grade)
+    {
+        var intResult = (int)Math.Round(grade);
+        AddGrade(intResult);
+    }
+}
---
+++ FilmChallengeApp/FilmChallengeApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using FilmChallengeApp;
+
+Console.WriteLine("Welcome in Rating Movie Program!\n");
+
+var film = new Film("Iron Man", 2008);
+
+do
+{
+    Console.WriteLine($"Add grade to {film.Title} ({film.Year}):");
+    var choise = Console.ReadLine();
+
+    if(choise.Trim() == "q" || choise.Trim() == "Q")
+    {
+        break;
+    }
+    try
+    {
+        film.AddGrade(choise);
+    }
+    catch (FormatException fe)
+    {
+        Console.WriteLine(fe.Message);
+    }
+    catch (ArgumentException ae)
+    {
+        Console.WriteLine(ae.Message);
+    }
+} while (true);
