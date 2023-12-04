c536bc979e05afe555b152a447c5e88ff010a77b
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-12-04 15:48:52 +0100

    Added saveing choise in Program class

--- FilmChallengeApp/FilmChallengeApp/Program.cs
+++ FilmChallengeApp/FilmChallengeApp/Program.cs
@@ -1,26 +1,43 @@
 ﻿using FilmChallengeApp;
 
 Console.WriteLine("Welcome in Rating Movie Program!\n");
-
-var film = AddFilm();
-
-AddGrade(film);
-
-try
+do
 {
-    var statistics = film.GetStatistics();
-    Console.WriteLine($"Statistics for {film.Title} ({film.Year}):");
-    Console.WriteLine($"\tMin added grade: {statistics.Min}");
-    Console.WriteLine($"\tMax added grade: {statistics.Max}");
-    Console.WriteLine($"\tAverage of all grades: {statistics.Average}");
-    Console.WriteLine($"\tAverage Letter shows quality of movie: {statistics.AverageLetter}");
-}
-catch (FileNotFoundException fe)
+    var choise = "";
+    try
+    {
+        choise = ChooseMemoryOrFile();
+    }
+    catch (ArgumentException ae)
+    {
+        Console.WriteLine($"Exception catched: {ae.Message}");
+    }
+
+    if (choise == "1" || choise == "2")
+    {
+        break;
+    }
+} while (true);
+
+static FilmInMemory AddFilmToMemory()
 {
-    Console.WriteLine(fe.Message);
+    Console.Write("\tInsert title of movie: ");
+    var title = Console.ReadLine();
+    Console.Write("\tInsert year of movie: ");
+    var year = Console.ReadLine();
+    do
+    {
+        if (int.TryParse(year, out int intYear))
+        {
+            return new FilmInMemory(title, int.Parse(year));
+        }
+        Console.WriteLine("\t\tWrong year! Try again...");
+        Console.Write("\tInsert year of movie again: ");
+        year = Console.ReadLine();
+    } while (true);
 }
 
-static FilmInFile AddFilm()
+static FilmInFile AddFilmToFile()
 {
     Console.Write("\tInsert title of movie: ");
     var title = Console.ReadLine();
@@ -38,7 +55,7 @@ static FilmInFile AddFilm()
     } while (true);
 }
 
-static void AddGrade(FilmInFile film)
+static void AddGradeInMemory(FilmInMemory film)
 {
     do
     {
@@ -55,15 +72,89 @@ static void AddGrade(FilmInFile film)
         }
         catch (FormatException fe)
         {
-            Console.WriteLine(fe.Message);
+            Console.WriteLine($"Exception catched: {fe.Message}");
         }
         catch (ArgumentException ae)
         {
-            Console.WriteLine(ae.Message);
+            Console.WriteLine($"Exception catched: {ae.Message}");
+        }
+    } while (true);
+}
+
+static void AddGradeInFile(FilmInFile filmInFile)
+{
+    do
+    {
+        Console.WriteLine($"Add grade to {filmInFile.Title} ({filmInFile.Year}):");
+        var choise = Console.ReadLine();
+
+        if (choise.Trim() == "q" || choise.Trim() == "Q")
+        {
+            break;
+        }
+        try
+        {
+            filmInFile.AddGrade(choise);
+        }
+        catch (FormatException fe)
+        {
+            Console.WriteLine($"Exception catched: {fe.Message}");
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine($"Exception catched: {ae.Message}");
         }
         catch (FileNotFoundException fe)
         {
-            Console.WriteLine(fe.Message);
+            Console.WriteLine($"Exception catched: {fe.Message}");
         }
     } while (true);
+}
+
+static string ChooseMemoryOrFile()
+{
+    Console.WriteLine("Where do you want save grade?");
+    Console.WriteLine("\t1 - Save in memory");
+    Console.WriteLine("\t2 - Save in file");
+    Console.Write("Your choise is: ");
+    var choise = Console.ReadLine();
+
+    switch (choise)
+    {
+        case "1":
+            Console.WriteLine("\tNow you are saveing in memory");
+            var filmInMemory = AddFilmToMemory();
+
+            AddGradeInMemory(filmInMemory);
+
+            var statistics = filmInMemory.GetStatistics();
+            Console.WriteLine($"Statistics for {filmInMemory.Title} ({filmInMemory.Year}):");
+            Console.WriteLine($"\tMin added grade: {statistics.Min}");
+            Console.WriteLine($"\tMax added grade: {statistics.Max}");
+            Console.WriteLine($"\tAverage of all grades: {statistics.Average}");
+            Console.WriteLine($"\tAverage Letter shows quality of movie: {statistics.AverageLetter}");
+            return "1";
+        case "2":
+            Console.WriteLine("\tNow you are saveing in file");
+            var filmInFile = AddFilmToFile();
+
+            AddGradeInFile(filmInFile);
+
+            try
+            {
+                statistics = filmInFile.GetStatistics();
+                Console.WriteLine($"Statistics for {filmInFile.Title} ({filmInFile.Year}):");
+                Console.WriteLine($"\tMin added grade: {statistics.Min}");
+                Console.WriteLine($"\tMax added grade: {statistics.Max}");
+                Console.WriteLine($"\tAverage of all grades: {statistics.Average}");
+                Console.WriteLine($"\tAverage Letter shows quality of movie: {statistics.AverageLetter}");
+            }
+            catch (FileNotFoundException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+            return "2";
+        default:
+            throw new ArgumentException($"Invalid value: '{choise}'! Try again. It's simple, choose 1 or 2.");
+    }
 }
